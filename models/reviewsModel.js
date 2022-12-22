b8ad9a3c63c9790fa089ec05f26fa7cf9382e7f2
amarupd <amarduttupadhyay@gmail.com>
2022-12-22 15:13:36 +0530

    ggergf

---
+++ models/reviewsModel.js
@@ -0,0 +1,11 @@
+module.exports=(sequelize,DataTypes)=>{
+    const Review=sequelize.define("review",{
+        rating:{
+            type:DataTypes.FLOAT
+        },
+        description:{
+            type:DataTypes.TEXT
+        }
+    });
+    return Reviews;
+}
