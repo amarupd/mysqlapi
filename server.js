d83079240814646fe32598ccfb95097166da9c9d
amarupd <amarduttupadhyay@gmail.com>
2022-12-22 13:31:12 +0530

    gdfssf

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 
-const PORT=process.env.PORT || 8000;
+const port=process.env.PORT || 8000;
 
 const app = express();
 
 var corOptions = {
-    origin: "https://localhost:5000"
-}
+    origin: 'https://localhost:8001'
+};
 
 //middleware
 
-app.use(cors(corOptions))
+app.use(cors(corOptions));
 
-app.use(express.json)
+app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    res.json({ message: "hello from api" })
-})
+app.get("", (req, res) => {
+    res.json({ message: 'hello from api' });
+});
 
 //port
 
-app.listen(PORT,()=>{
-    console.log(`listening to port number : ${PORT}`);
-})
+app.listen(port,()=>{
+    console.log(`listening to port number : ${port}`);
+});
