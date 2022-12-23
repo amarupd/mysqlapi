6cda83981f20eeb5bb7bf3f1471c359a82b8d296
amarupd <amarduttupadhyay@gmail.com>
2022-12-23 12:53:05 +0530

    dfdf

--- controller/reviewController.js
+++ controller/reviewController.js
@@ -16,3 +16,11 @@ const addReview=async(req,res)=>
     const review=await Review.create(data)
     res.status(200).send(review)
 }
+
+
+//get all the reviews
+
+const getAllReviews=async(req,res)=>{
+    const reviews=await Review.findAll({})
+    res.status(200).send(review)
+}
