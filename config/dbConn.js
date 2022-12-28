2889bf633f0a33a72aa231406da9f52b429fe7c9
amarupd <amarduttupadhyay@gmail.com>
2022-12-28 14:56:53 +0530

    sdsd

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,14 +1,14 @@
-module.exports={
-  host     : 'localhost',
-  user     : 'root',
-  password : 'amar',
-  database : 'node_mysql',
-  dialect : 'mysql',
+module.exports = {
+  host: 'localhost',
+  user: 'root',
+  password: 'amar',
+  database: 'node_mysql',
+  dialect: 'mysql',
 
-  pool:{
-    max:5,
-    min:0,
-    acquire:30000,
-    idle:10000
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
   }
 }
